1eb508e3e43e7289965c47812e863d4bbf7c99c5
Dzjkb <jakub.pies@gmail.com>
2016-12-07 20:45:55 +0100

    lmao

--- connect.py
+++ connect.py
@@ -2,9 +2,13 @@
 
 import socket
 import sys
+import threading
 
 from threading import Thread
-from time import sleep, strftime
+from time import strftime
+
+irc = socket.socket()
+displaymutex = threading.Lock()
 
 def get_input():
 	inp = []
@@ -37,11 +41,69 @@ def send(sock, msg):
 			raise RuntimeError("Cannot send, connection broken!")
 		totalsent += sent
 
+def sendthread():
+	userinput = ''
+	currentchannel = ''
+	run = True
+	while run:
+		print(currentchannel +'> ', end="")
+		userinput = input()
+		if len(userinput) == 0: userinput = ' '
+		if userinput[0] == '/': #command mode
+			userinput = userinput[1:]
+			userinput = userinput.split()
+			if userinput[0] == 'join':
+				print('Joining ' + userinput[1] + '...')
+				send(irc, 'JOIN #' + userinput[1] + '\r\n')
+				currentchannel = '#' + userinput[1]
+
+			elif userinput[0] == 'leave':
+				if currentchannel == '':
+					print('Currently not in any channel')
+				else:
+					print('Leaving #' + currentchannel + '...')
+					send(irc, 'QUIT\r\n')
+
+			elif userinput[0] == 'quit':
+				print('Bye bye :(')
+				run = False
+
+		elif '#' not in currentchannel:
+			print('No channel joined, cant send')
+		else:
+			send(irc, 'PRIVMSG ' + currentchannel + ' :' + userinput + '\r\n')
+
+	irc.close()
+	exit()
+
+def recvthread():
+	bufff = b''
+	while True:
+		if b'\r\n' not in bufff:
+			try:
+				bufff += irc.recv(512)
+			except:
+				print('Connection aborted')
+				raise
+
+			if bufff == b'':
+				print('Connection closed')
+				raise
+
+			continue
+
+		time = '[%s]' % strftime('%H:%M')
+		line, bufff = bufff.split(b'\r\n', 1)
+		if b'PING' in line:
+			send(irc, 'PONG ' + line[5:].decode('utf-8'))
+
+		print(time + line.decode('utf-8'))
+
 # parsing the input; for simplicity all names are set to the entered nickname
 
 #inplist = get_input()
 ADDRESS = 'irc.freenode.net:6667' #inplist[0]
-NAME = 'ciulerson2' #inplist[1]
+NAME = 'ciulerson2bot' #inplist[1]
 ADDRESS = ADDRESS.split(sep=':')
 
 try:
@@ -58,8 +120,6 @@ except:
 
 ADDRESS = tuple(ADDRESS)
 print(ADDRESS)
-
-irc = socket.socket()
 
 try:
 	irc.connect(ADDRESS)
@@ -91,54 +151,8 @@ while dupa:
 
 	print(line.decode('utf-8'))
 
-def sendthread():
-	userinput = ''
-	currentchannel = 'dupsko'
-	while userinput != 'quit':
-		print(currentchannel +'> ', end="")
-		userinput = input()
-		if len(userinput) == 0: userinput = ' '
-		if userinput[0] == '/': #command mode
-			userinput = userinput[1:]
-			userinput = userinput.split()
-			if userinput[0] == 'join':
-				print('Joining ' + userinput[1] + '...')
-				send(irc, 'JOIN #' + userinput[1] + '\r\n')
-				currentchannel = '#' + userinput[1]
-
-		elif '#' not in currentchannel:
-			print('No channel joined, cant send')
-		elif userinput != 'quit':
-			send(irc, 'PRIVMSG ' + currentchannel + ' :' + userinput + '\r\n')
-
-	irc.close()
-	exit()
-
-def recvthread():
-	bufff = b''
-	while True:
-		if b'\r\n' not in bufff:
-			try:
-				bufff += irc.recv(512)
-			except:
-				print('Connection aborted')
-				raise
-
-			if bufff == b'':
-				print('Connection closed')
-				raise
-
-			continue
-
-		time = '[%s]' % strftime('%H:%M')
-		line, bufff = bufff.split(b'\r\n', 1)
-		if b'PING' in line:
-			send(irc, 'PONG ' + line[5:].decode('utf-8'))
-
-		print('\033[s\033[2A\n' + time + line.decode('utf-8') + '\033[u', end="")
-
 send_thread = Thread(target=sendthread)
-recv_thread = Thread(target=recvthread)
+recv_thread = Thread(target=recvthread, daemon=True)
 
 recv_thread.start()
 send_thread.start()
